043dba6b0013d4bf149d2ab47e613c6104b91f5f
Anne Drottning <AnneDrottning@Annes-MacBook-Pro-147.local>
2019-12-15 19:09:59 +0100

    Created folders for images and sound, and finished the template

--- main.py
+++ main.py
@@ -15,5 +15,32 @@ BLUE  = (  0,   0, 255)
 # Initialize pygame and create the window
 pygame.init()
 pygame.mixer.init()
-screen = pygame.set_caption("Platform Jumping")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Platform Jumping")
 clock  = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+
+# Game loop
+running = True
+while running:
+    # Keep loop running at the right speed
+    clock.tick(FPS)
+
+    # Process input events:
+    for event in pygame.event.get():
+        # Check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update:
+    all_sprites.update()
+
+    # Draw / render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+
+    # After drawing everything, we flip the display
+    pygame.display.flip()
+
+pygame.quit()
