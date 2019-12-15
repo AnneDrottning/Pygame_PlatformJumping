aec7a83e56d922a0f267c5b8c2d3bcc4a4c2b8fa
Anne Drottning <AnneDrottning@Annes-MacBook-Pro-147.local>
2019-12-15 19:02:53 +0100

    Start on the game code

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import pygame
 import random
+
+WIDTH  = 360
+HEIGHT = 480
+FPS    =  30
+
+# Define the colours we want to use:
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+RED   = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE  = (  0,   0, 255)
+
+# Initialize pygame and create the window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.set_caption("Platform Jumping")
+clock  = pygame.time.Clock()
